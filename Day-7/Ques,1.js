c97b4238d39c05e575bc9b017dc62b099829126d
ritugupta02 <85019207+ritugupta02@users.noreply.github.com>
2021-06-15 15:44:10 +0530

    THA-7 Added

---
+++ Day-7/Ques,1.js
@@ -0,0 +1,14 @@
+/*  1. Write a JavaScript program to list the properties of a JavaScript object.
+ Sample object: var student = { name : "David Rayy", sclass : "VI", rollno : 12 };
+  Sample Output: name,sclass,rollno
+*/
+
+var student =
+{
+    name: "David Rayy",
+    sclass: "VI",
+    rollno: 12
+};
+
+var x = Object.getOwnPropertyNames(student);
+console.log(x.join(','));
