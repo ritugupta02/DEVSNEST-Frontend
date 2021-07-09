98759d7115693c85fa934143811289454ae5c1d3
ritugupta02 <85019207+ritugupta02@users.noreply.github.com>
2021-07-09 21:27:09 +0530

    Card added

---
+++ Day-16/src/card.jsx
@@ -0,0 +1,10 @@
+import "./index.css";
+const Card = () => {
+    return (<div className="card" >
+        <img src="react-meme1_.png" alt=" " />
+        <h1>React card</h1>
+        <h3>Meme card using Reactjs</h3>
+    </div>);
+};
+
+export default Card;
---
+++ Day-16/src/meme.jsx
@@ -0,0 +1,14 @@
+import Card from "./card";
+
+function App() {
+    return (
+        <div className="App">
+            <Card />
+            <Card />
+            <Card />
+
+        </div>
+    );
+}
+
+export default App;
