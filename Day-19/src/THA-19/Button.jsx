98ac34aff376c20eab55e5348977b2a4b3f22deb
ritugupta02 <85019207+ritugupta02@users.noreply.github.com>
2021-07-16 14:15:34 +0530

    changed

--- Day-19/src/THA-19/Button.jsx
+++ Day-19/src/THA-19/Button.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import "./button.css";
 const Uhook = () => {
     const [count, setCount] = useState(0);
+    const INC = () => {
+        setCount(count + 1);
 
+    };
     return (
 
 
-        <button className="btn" onClick={() => setCount(count + 1)}>
+        <button className="btn" onClick={INC} >
             you clicked on me {count} times!
         </button>
 
